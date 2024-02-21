3e6319c2816af30e15c412bedd7c90600ac484aa
amolnarlawar <91715308+amolnarlawar@users.noreply.github.com>
2024-02-21 21:49:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "ami" {
 variable "instance_type" {
    type        = string
    description = "Instance type"
-   default     = "t2.micro"
+   default     = "t2.nano"
 }
 
 variable "name_tag" {
